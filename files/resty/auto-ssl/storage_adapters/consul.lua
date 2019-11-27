9d5d15abac9f8f2d6f2bc03372cbe4c3991ac4f9
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-11-27 18:11:35 -0300

    lua-resty-auto-ssl-storage-adapter-consul (#24): maybe /usr/local/share/lua/5.1/resty/auto-ssl/storage_adapters/consul.lua would be the the path to create a new storage_adapter? Humm

---
+++ files/resty/auto-ssl/storage_adapters/consul.lua
@@ -0,0 +1,12 @@
+#!/usr/bin/env lua
+
+-- I think the path would be /usr/local/share/lua/5.1/resty/auto-ssl/storage_adapters/consul.lua
+-- to work with resty/auto-ssl
+-- And then use this as reference https://github.com/GUI/lua-resty-auto-ssl/blob/master/lib/resty/auto-ssl/storage_adapters/redis.lua
+
+-- Lean lua in an Hour https://www.youtube.com/watch?v=S4eNl1rA1Ns
+-- Definitely an openresty guide/ Hello world https://www.staticshin.com/programming/definitely-an-open-resty-guide/#hello_world
+-- Lua in 15 minutes http://tylerneylon.com/a/learn-lua/
+
+
+print("Hello World")
