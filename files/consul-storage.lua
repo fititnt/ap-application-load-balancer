4d477e4ca1cadb577578ace10cfe603c9838ec68
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-11-27 15:08:43 -0300

    lua-tests (#24): hello world

---
+++ files/consul-storage.lua
@@ -0,0 +1,6 @@
+#!/usr/bin/env lua
+
+-- Lean lua in an Hour https://www.youtube.com/watch?v=S4eNl1rA1Ns
+-- Definitely an openresty guide/ Hello world https://www.staticshin.com/programming/definitely-an-open-resty-guide/#hello_world
+
+print("Hello World")
