f5ce432c1867271c1461bff281a5ff657f8cc55b
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-12-25 20:18:15 -0300

    ansible-molecule,travis (#2), alb-on-docker (#43): Added var alb_node_isconteiner

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,3 +13,17 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_haproxy_is_installed_and_running(host):
+    haproxy = host.package("haproxy")
+
+    assert haproxy.is_installed
+    assert haproxy.is_running
+
+
+def test_openresty_is_installed_and_running(host):
+    openresty = host.package("openresty")
+
+    assert openresty.is_installed
+    assert openresty.is_running
