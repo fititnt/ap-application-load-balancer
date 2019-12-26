ad0d8933deeaf32ce05fae2ed57fc4796020c60c
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-12-25 22:41:29 -0300

    ansible-molecule,travis (#2), allow travis ignore build fails (some of these will be re-enabled later; but less tested OSs without reliable Docker image will take more time)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -19,11 +19,35 @@ def test_haproxy_is_installed_and_running(host):
     haproxy = host.package("haproxy")
 
     assert haproxy.is_installed
-    assert haproxy.is_running
+    # assert haproxy.is_running
+    # Error: E       AttributeError: 'DebianPackage' object has no attribute
+    # 'is_running'
 
 
 def test_openresty_is_installed_and_running(host):
     openresty = host.package("openresty")
 
     assert openresty.is_installed
-    assert openresty.is_running
+    # assert openresty.is_running
+    # Error: E       AttributeError: 'DebianPackage' object has no attribute
+    # 'is_running'
+
+# From https://testinfra.readthedocs.io/en/latest/#quick-start
+# def test_passwd_file(host):
+#     passwd = host.file("/etc/passwd")
+#     assert passwd.contains("root")
+#     assert passwd.user == "root"
+#     assert passwd.group == "root"
+#     assert passwd.mode == 0o644
+#
+#
+# def test_nginx_is_installed(host):
+#     nginx = host.package("nginx")
+#     assert nginx.is_installed
+#     assert nginx.version.startswith("1.2")
+#
+#
+# def test_nginx_running_and_enabled(host):
+#     nginx = host.service("nginx")
+#     assert nginx.is_running
+#     assert nginx.is_enabled
