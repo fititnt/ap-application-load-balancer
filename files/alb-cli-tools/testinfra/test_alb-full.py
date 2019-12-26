07e09388c76594d92dc92b2ad5edb863f9ebcd9b
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-12-26 00:57:58 -0300

    testinfra (#44): started

---
+++ files/alb-cli-tools/testinfra/test_alb-full.py
@@ -0,0 +1,30 @@
+def test_passwd_file(host):
+    passwd = host.file("/etc/passwd")
+    assert passwd.contains("root")
+    assert passwd.user == "root"
+    assert passwd.group == "root"
+    assert passwd.mode == 0o644
+
+
+def test_openresty_is_installed(host):
+    nginx = host.package("openresty")
+    assert nginx.is_installed
+    # assert nginx.version.startswith("1.2")
+
+
+def test_openresty_running_and_enabled(host):
+    nginx = host.service("openresty")
+    assert nginx.is_running
+    assert nginx.is_enabled
+
+
+def test_haproxy_is_installed(host):
+    nginx = host.package("haproxy")
+    assert nginx.is_installed
+    # assert nginx.version.startswith("1.2")
+
+
+def test_haproxy_running_and_enabled(host):
+    nginx = host.service("haproxy")
+    assert nginx.is_running
+    assert nginx.is_enabled
