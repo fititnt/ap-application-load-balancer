496b26423e917ce7efca5e4466f5ff75cc7f038d
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-12-27 01:04:50 -0300

    testinfra (#44): mananged components now create simple file at /opt/alb/info/ to allow tests know what to check

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,45 +7,46 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root' or f.group == 'wheel'
+def test_required_haproxy_is_installed(host):
+    assert (not host.file("/opt/alb/info/haproxy").exists) \
+        or host.package("haproxy").is_installed
 
 
-def test_haproxy_is_installed(host):
-    assert host.package("haproxy").is_installed
+def test_required_haproxy_is_enabled(host):
+    assert (not host.file("/opt/alb/info/haproxy").exists) \
+        or host.service("haproxy").is_enabled
 
 
-def test_haproxy_is_enabled(host):
-    assert host.service("haproxy").is_enabled
+def test_required_haproxy_is_running(host):
+    assert (not host.file("/opt/alb/info/haproxy").exists) \
+        or host.service("haproxy").is_running
 
 
-def test_haproxy_is_running(host):
-    assert host.service("haproxy").is_running
+def test_required_openresty_is_installed(host):
+    assert (not host.file("/opt/alb/info/openresty").exists) \
+        or host.package("openresty").is_installed
 
 
-def test_openresty_is_installed(host):
-    assert host.package("openresty").is_installed
+def test_required_openresty_is_enabled(host):
+    assert (not host.file("/opt/alb/info/openresty").exists) \
+        or host.service("openresty").is_enabled
 
 
-def test_openresty_is_enabled(host):
-    assert host.service("openresty").is_enabled
+def test_required_openresty_is_running(host):
+    assert (not host.file("/opt/alb/info/openresty").exists) \
+        or host.service("openresty").is_running
 
 
-def test_openresty_is_running(host):
-    assert host.service("openresty").is_running
+def test_required_ufw_is_installed(host):
+    assert (not host.file("/opt/alb/info/ufw").exists) \
+        or host.package("ufw").is_installed
 
 
-#def test_ufw_is_installed(host):
-#    assert host.package("ufw").is_installed
-#
-#
-#def test_ufw_is_enabled(host):
-#    assert host.service("ufw").is_enabled
-#
-#
-#def test_ufw_is_running(host):
-#    assert host.service("ufw").is_running
+def test_required_ufw_is_enabled(host):
+    assert (not host.file("/opt/alb/info/ufw").exists) \
+        or host.service("ufw").is_enabled
+
+
+def test_required_ufw_is_running(host):
+    assert (not host.file("/opt/alb/info/ufw").exists) \
+        or host.service("ufw").is_running
